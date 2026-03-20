558ef8a8b2a1ccf6b319797a446cf6caa9c5d779
akanefx99 <akanefx99@gmail.com>
2026-03-20 07:10:59 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import connectToWhatsapp from './Digix/crew.js'
+
 import handleIncomingMessage from './events/messageHandler.js'
 
 (async() => {
+
     await connectToWhatsapp(handleIncomingMessage)
-        console.log('established !')
+
+    console.log('established !')
+
 })()
