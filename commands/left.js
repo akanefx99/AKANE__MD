82c448927e7faccd04d5345039e9cf4c1bd55723
akanefx99 <akanefx99@gmail.com>
2026-03-25 20:14:30 +0000

    Create left.js

---
+++ commands/left.js
@@ -0,0 +1,31 @@
+// commands/left.js
+
+// @cat: gc-menu
+
+// Quitter un groupe
+
+async function leftCommand(client, message, args) {
+
+    const remoteJid = message.key.remoteJid;
+
+    const isGroup = remoteJid.includes('g.us');
+
+    
+
+    if (!isGroup) {
+
+        await client.sendMessage(remoteJid, { text: "> *Cette commande n'est disponible que dans un groupe idiot ðŸ’ !*" });
+
+        return;
+
+    }
+
+    
+
+    await client.sendMessage(remoteJid, { text: "> *_A DIOS BANDE DE LOSER_ ðŸ’*" });
+
+    await client.groupLeave(remoteJid);
+
+}
+
+export default leftCommand;
